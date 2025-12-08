2fa6e3690b4e9dbd1391385164b2d39584232de0
Manishk5507 <7055ranamanish@gmail.com>
2025-12-08 19:21:43 +0530

    Fix some more issues related to deployment

--- api/index.ts
+++ api/index.ts
@@ -9,11 +9,6 @@ import { topicsRoute } from "../server/routes/topics";
 import { ragRoute } from "../server/routes/rag";
 import { lessonPlansRoute } from "../server/routes/lessonPlans";
 
-// Set runtime to edge for better performance (optional, can use nodejs too)
-export const config = {
-  runtime: "nodejs",
-};
-
 const app = new Hono().basePath("/api");
 
 // Enable CORS
@@ -21,12 +16,9 @@ app.use("*", cors());
 app.use("*", logger());
 
 // Mount routes
-const apiRoutes = app
-  .route("/topics", topicsRoute)
-  .route("/rag", ragRoute)
-  .route("/lessonPlans", lessonPlansRoute)
-  .route("/", authRoute);
+app.route("/topics", topicsRoute);
+app.route("/rag", ragRoute);
+app.route("/lessonPlans", lessonPlansRoute);
+app.route("/", authRoute);
 
 export default handle(app);
-export type ApiRoutes = typeof apiRoutes;
-
